3d18c0f908961ba7fdd6ea6b02cf74a0b8fed3e6
Ford23 <admirsitnic@gmail.com>
2018-07-16 23:33:07 +0200

    Models

---
+++ AT/src/Model/Adress.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class Adress {
+    private  String adress;
+    private  String postalCode;
+    private  String town;
+    private  String city;
+}
---
+++ AT/src/Model/Hotel.java
@@ -0,0 +1,9 @@
+package Model;
+import  Model.Adress;
+
+public class Hotel{
+    private  int Id;
+    private String hotelName;
+    private Adress hotelAdress;
+
+}
