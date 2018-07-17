549218325e42ab4bd81b13a54193878f8e1f3336
unknown <b8emir@gmail.com>
2018-07-17 02:04:49 +0200

    Changes

--- src/main/java/at3/demo/Model/Hotel.java
+++ src/main/java/at3/demo/Model/Hotel.java
@@ -16,47 +16,44 @@ public class Hotel{
     private String contactNumber;
     private float roomPrice;
 
-    public Hotel(String hotelName,String hotelAdress,int longitude,int latitude)
+    public Hotel(String hotelName,String hotelAdress,double longitude,double latitude,float roomPrice)
     {
         this.hotelName=hotelName;
         this.hotelAdress=hotelAdress;
         this.longitude=longitude;
         this.latitude=latitude;
-
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public void setLongitude(int longitude) {
-        this.longitude = longitude;
+        this.roomPrice = roomPrice;
     }
 
     //Setters
+    protected void setId(Long id) {
+        this.Id = id;
+    }
+
     protected void setHotelName(String hotelName){
         this.hotelName = hotelName;
     }
     protected void setHotelAdress(String hotelAdress){
         this.hotelAdress = hotelAdress;
     }
-    protected void setlongitude(int longitude){
+    protected void setlongitude(double longitude){
         this.longitude= longitude;
     }
-    protected void setLatitude(int latitude){
+    protected void setLatitude(double latitude){
         this.latitude = latitude;
     }
     protected void setContactNumber(String contactNumber){
         this.contactNumber = contactNumber;
     }
+    protected void setRoomPrice(float roomPrice){
+        this.roomPrice = roomPrice;
+    }
     //Getters
+    public Long getId() {return Id};
     public String getHotelName(){return hotelName;}
     public String getHotelAdress(){return hotelAdress;}
     public double getLongitude(){return longitude;}
     public double getLatitude(){return latitude;}
     public String getContactNumber(){return contactNumber;}
+    public float getRoomPrice(){return roomPrice;}
 }
