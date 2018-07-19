da28f1e6f45ae87ebfb217171cdb3ae5e14479fb
unknown <b8emir@gmail.com>
2018-07-19 16:22:26 +0200

    Controller modifications

--- src/main/java/at3/demo/Controller/ReservationController.java
+++ src/main/java/at3/demo/Controller/ReservationController.java
@@ -1,37 +1,72 @@
 package at3.demo.Controller;
 
+import at3.demo.Model.Hotel;
 import at3.demo.Model.Reservation;
-
+import at3.demo.Repository.HotelRepository;
 import at3.demo.Repository.ReservationRepository;
-import at3.demo.Repository.ReservationUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
+@RequestMapping(value = "/reservations")
 public class ReservationController {
     @Autowired
-    private ReservationRepository reservationRepository;
-
-    @Autowired
-
-    private ReservationUserRepository reservationUserRepository;
+    public ReservationRepository reservations;
 
     /*@GetMapping
     public Page<Reservation> getAllReservation(@PathVariable Long userId, Pageable pageable) {
         return reservationRepository.findByPostId(userId, pageable);
     }*/
 
-    @RequestMapping
+    @RequestMapping(value = "/{reservation_id}", method = RequestMethod.GET)
     public Optional<Reservation> getReservationById(Long Id) {
         try {
-            return reservationRepository.findById(Id);
+            return reservations.findById(Id);
 
         } catch (Exception e) {
             System.out.println("Error!!!");
             return null;
         }
+    }
+
+    @RequestMapping(value = "/getreservation", method = RequestMethod.GET)
+    public Page<Reservation> getAllReservation(Pageable pageable){
+        try {
+            return reservations.findAll(pageable);
+
+        } catch (Exception e) {
+            System.out.println("Error!!!");
+            return null;
+        }
+    }
+
+    @RequestMapping(value = "/deletereservation", method = RequestMethod.DELETE)
+    public void removeById(Pageable pageable, Reservation hotel){
+        try{
+            reservations.deleteById(hotel.getId());
+            reservations.save(hotel);
+        }catch(Exception e){
+            System.out.println("Error 200!Couldn't find that Id!");
+        }
+    }
+
+    @RequestMapping(value = "/updatereservation", method = RequestMethod.PUT)
+    public void updateHotel(@Valid @RequestBody Reservation reservation){
+        HotelRepository existingHotel;
+        Optional<Reservation>newReservation = this.reservations.findById(reservation.getId());
+        reservations.save(reservation);
+    }
+
+    @RequestMapping(value = "/addreservation", method = RequestMethod.POST)
+    public void addHotel(@Valid @RequestBody Reservation reservation){
+        reservations.save(reservation);
     }
 }
